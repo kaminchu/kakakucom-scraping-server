d3080a0d50554e6b2912b1e9cc1c7c9d3cf1f885
kaminchu <kmchu.mail@gmail.com>
2018-12-02 16:39:20 +0900

    cors対応

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,13 @@ app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// CORSを許可する
+app.use(function(_, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
 app.use("/api", apiRouter);
 
 export default app;
